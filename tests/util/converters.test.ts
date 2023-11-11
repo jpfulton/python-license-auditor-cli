883cbf5c6a17c327d91fd4d9c87721727105714c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-11-11 15:32:59 -0600

    Update converters to address full text license properties.

--- tests/util/converters.test.ts
+++ tests/util/converters.test.ts
@@ -111,6 +111,70 @@ describe("convertPackageInfo", () => {
 
     expect(actualDependency).toEqual(expectedDependency);
   });
+
+  it("should return a Dependency object with license 'FULL TEXT' if license is longer than 50 characters", () => {
+    const licenseFullText = `BSD 3-Clause License (Revised)
+        
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+    
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+    
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+    
+    3. Neither the name of the project nor the names of its contributors
+       may be used to endorse or promote products derived from this software
+       without specific prior written permission.
+    
+    THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+    OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+    OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+    SUCH DAMAGE.`;
+
+    const packageInfo = {
+      metadata: {
+        metadata_version: "2.1",
+        name: "six",
+        version: "1.15.0",
+        summary: "Python 2 and 3 compatibility utilities",
+        home_page: "https://www.google.com",
+        platform: ["UNKNOWN"],
+        license: licenseFullText,
+      },
+    } as PackageInfo;
+
+    const rootProjectName = "test";
+
+    const expectedDependency = {
+      rootProjectName: rootProjectName,
+      name: "six",
+      version: "1.15.0",
+      publisher: "",
+      repository: "",
+      licenses: [
+        {
+          license: "FULL TEXT",
+          url: "https://www.google.com",
+          fullText: licenseFullText,
+        },
+      ],
+    } as Dependency;
+
+    const actualDependency = convertPackageInfo(packageInfo, rootProjectName);
+
+    expect(actualDependency).toEqual(expectedDependency);
+  });
 });
 
 describe("getLicensesFromMetadata", () => {
